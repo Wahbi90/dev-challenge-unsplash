c3a797cf4ee9691e3e6b5c4da9a5dad645a27289
Wahbi90 <ben.o.abdelwahab@gmail.com>
2021-01-19 14:29:22 +0000

    init client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "./Component/Title"
+import UploadForm from "./Component/UploadForm";
+
+function App () {
+    return (
+        <div>
+            <Title/>
+            <UploadForm />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/Component/UploadForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const UploadForm = () => {
+    
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ['image/png', 'image/jpg', 'image/jpeg']
+
+    const changeHandler = (e) => {
+        let selected =e.target.files[0];
+        console.log(selected)
+        if (selected && types.includes(selected.type)){
+            setFile(selected);
+            setError('');
+        }else{
+           setFile(null);
+           setError("please select an image file (png, jpg or jpeg)");
+        }
+    }
+
+    return (
+        <form>
+            <input type="file" onChange = {changeHandler}/>
+            <div className="output">
+                { error && <div className = "error">{ error }</div> }
+                { file && <div>{ file.name }</div> }
+            </div>
+        </form>
+    );
+}
+
+export default UploadForm;
